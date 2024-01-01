e4ffc79155a4157a9d18c4b81db2fb445622b5ee
Sebastien Robitaille <sebastien@robitaille.info>
2023-12-31 21:10:15 -0800

    Add project files.

---
+++ BlazorMQTTTestingWASM/Program.cs
@@ -0,0 +1,12 @@
+using BlazorMQTTTestingWASM;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<MQTTnet.ClientLib.MqttService>();
+
+await builder.Build().RunAsync();
