107be95a5b45819b73697801195cea510ea2a102
Kayleb814 <kayleb_s@hotmail.com>
2024-09-01 16:14:11 -0700

    Added Lighting sliders to Dashboard

---
+++ BlazorMQTTTestingWASM/DualColorLEDLightingDevice.cs
@@ -0,0 +1,41 @@
+﻿using BlazorMQTTTestingWASM.Models;
+
+namespace BlazorMQTTTestingWASM
+{
+    public class DualColorLEDLightingDevice : Device
+    {
+        private int _WhiteLED = 0;
+        private int _BlueLED = 0;
+        public int WhiteLED
+        {
+            get
+            {
+                return _WhiteLED;
+            }
+            set
+            {
+                _WhiteLED = value;
+                sendMessageData(nodeID, 2560, (ulong)_WhiteLED);
+            }
+        }
+
+        public int BlueLED
+        {
+            get
+            {
+                return _BlueLED;
+            } 
+            set 
+            {
+                _BlueLED = value;
+                sendMessageData(nodeID, 2561, (ulong)_BlueLED);
+            } 
+        }
+        private int nodeID;
+        public DualColorLEDLightingDevice(MQTTnet.ClientLib.MqttService mqttService, int systemID, int basestationID, int nodeID) : base(mqttService, systemID, basestationID)
+        {
+            this.nodeID = nodeID;
+        }
+
+    }
+}
