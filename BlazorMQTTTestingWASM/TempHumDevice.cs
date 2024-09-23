88ada284845ece87de2d45241e8b7e0a352bbbea
Kayleb814 <kayleb_s@hotmail.com>
2024-09-22 19:01:06 -0700

    Added Fields and icons toi set Alarms for temps and hum

--- BlazorMQTTTestingWASM/TempHumDevice.cs
+++ BlazorMQTTTestingWASM/TempHumDevice.cs
@@ -7,6 +7,19 @@
         private float _CanopyHum = 0;
         private float _TankTemp = 0;
         private float _SumpTemp = 0;
+        private bool _CanopyTempAlarmOnOff = false;
+        private float _CanopyTempAlarmLow = 0;
+        private float _CanopyTempAlarmHigh = 0;
+        private bool _CanopyHumAlarmOnOff = false;
+        private float _CanopyHumAlarmLow = 0;
+        private float _CanopyHumAlarmHigh = 0;
+        private bool _TankTempAlarmOnOff = false;
+        private float _TankTempAlarmLow = 0;
+        private float _TankTempAlarmHigh = 0;
+        private bool _SumpTempAlarmOnOff = false;
+        private float _SumpTempAlarmLow = 0;
+        private float _SumpTempAlarmHigh = 0;
+
 
         public TempHumDevice(MQTTnet.ClientLib.MqttService mqttService, int systemID,int basestationID,int nodeID) : base(mqttService, systemID, basestationID)
         {
@@ -26,6 +39,45 @@
             }
         }
 
+        public bool CanopyTempAlarmOnOff
+        {
+            get
+            {
+                return _CanopyTempAlarmOnOff;
+            }
+            set
+            {
+                _CanopyTempAlarmOnOff = value;
+                sendMessageData(nodeID, 2564, (ulong)(_CanopyTempAlarmOnOff ? 1 : 0));
+            }
+        }
+
+        public float CanopyTempAlarmLow
+        {
+            get
+            {
+                return _CanopyTempAlarmLow;
+            }
+            set
+            {
+                _CanopyTempAlarmLow = value;
+                sendMessageData(nodeID, 2565, (ulong)_CanopyTempAlarmLow);
+            }
+        }
+
+        public float CanopyTempAlarmHigh
+        {
+            get
+            {
+                return _CanopyTempAlarmHigh;
+            }
+            set
+            {
+                _CanopyTempAlarmHigh = value;
+                sendMessageData(nodeID, 2566, (ulong)_CanopyTempAlarmHigh);
+            }
+        }
+
         public float CanopyHum
         {
             get
@@ -36,6 +88,45 @@
             {
                 _CanopyHum = value;
                 sendMessageData(nodeID, 2561, (ulong)_CanopyHum);
+            }
+        }
+
+        public bool CanopyHumAlarmOnOff
+        {
+            get
+            {
+                return _CanopyHumAlarmOnOff;
+            }
+            set
+            {
+                _CanopyHumAlarmOnOff = value;
+                sendMessageData(nodeID, 2567, (ulong)(_CanopyHumAlarmOnOff ? 1 : 0));
+            }
+        }
+
+        public float CanopyHumAlarmLow
+        {
+            get
+            {
+                return _CanopyHumAlarmLow;
+            }
+            set
+            {
+                _CanopyHumAlarmLow = value;
+                sendMessageData(nodeID, 2568, (ulong)_CanopyHumAlarmLow);
+            }
+        }
+
+        public float CanopyHumAlarmHigh
+        {
+            get
+            {
+                return _CanopyHumAlarmHigh;
+            }
+            set
+            {
+                _CanopyHumAlarmHigh = value;
+                sendMessageData(nodeID, 2569, (ulong)_CanopyHumAlarmHigh);
             }
         }
 
@@ -52,6 +143,47 @@
             }
         }
 
+        public bool TankTempAlarmOnOff
+        {
+            get
+            {
+                return _TankTempAlarmOnOff;
+            }
+            set
+            {
+                _TankTempAlarmOnOff = value;
+                sendMessageData(nodeID, 2569, (ulong)(_TankTempAlarmOnOff ? 1 : 0));
+            }
+        }
+
+        public float TankTempAlarmLow
+        {
+            get
+            {
+                return _TankTempAlarmLow;
+            }
+            set
+            {
+                _TankTempAlarmLow = value;
+                sendMessageData(nodeID, 2570, (ulong)_TankTempAlarmLow);
+            }
+        }
+
+        public float TankTempAlarmHigh
+        {
+            get
+            {
+                return _TankTempAlarmHigh;
+            }
+            set
+            {
+                _TankTempAlarmHigh = value;
+                sendMessageData(nodeID, 2571, (ulong)_TankTempAlarmHigh);
+            }
+        }
+
+
+
         public float SumpTemp
         {
             get
@@ -62,6 +194,45 @@
             {
                 _SumpTemp = value;
                 sendMessageData(nodeID, 2563, (ulong)_SumpTemp);
+            }
+        }
+
+        public bool SumpTempAlarmOnOff
+        {
+            get
+            {
+                return _SumpTempAlarmOnOff;
+            }
+            set
+            {
+                _SumpTempAlarmOnOff = value;
+                sendMessageData(nodeID, 2572, (ulong)(_SumpTempAlarmOnOff ? 1 : 0));
+            }
+        }
+
+        public float SumpTempAlarmLow
+        {
+            get
+            {
+                return _SumpTempAlarmLow;
+            }
+            set
+            {
+                _SumpTempAlarmLow = value;
+                sendMessageData(nodeID, 2573, (ulong)_SumpTempAlarmLow);
+            }
+        }
+
+        public float SumpTempAlarmHigh
+        {
+            get
+            {
+                return _SumpTempAlarmHigh;
+            }
+            set
+            {
+                _SumpTempAlarmHigh = value;
+                sendMessageData(nodeID, 2574, (ulong)_SumpTempAlarmHigh);
             }
         }
     }
